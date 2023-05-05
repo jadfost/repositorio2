bc0cb42560edc62cf813a6b3a94c981e1a740169
nirttson <stevenorduz@hotmail.com>
2023-05-04 21:27:24 -0500

    V1.2.3

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,9 @@
   <div class="container">
     <h1 class="title">Lista de personas</h1>
     <form class="add-form" @submit.prevent="agregarPersona">
-       
+        <h2>Agregar persona</h2>
+        <div class="form-group">
+          <label for="nombre">Nombre:</label>
+          <input type="text" id="nombre" v-model="nuevoNombre" required>
+        </div>
+        
