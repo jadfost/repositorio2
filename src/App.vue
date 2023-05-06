18008359a956d60dcc8c4e065f79bbbf02dbed04
stevenorduz <stevenorduz@hotmail.com>
2023-05-06 16:10:28 -0500

    V.4.3.2

--- src/App.vue
+++ src/App.vue
@@ -55,82 +55,81 @@
 
 <style>
 .container {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-  font-family: sans-serif;
-}
-
-.title {
-  margin-bottom: 20px;
-  text-align: center;
-  color: #0077CC;
-}
-
-.card {
-  background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
-  padding: 20px;
-}
-
-.list {
-  list-style: none;
-  padding: 0;
-}
-
-.list-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-  padding: 10px 0;
-}
-
-.list-item-content {
-  flex-grow: 1;
-}
-
-.btn-delete {
-  background-color: #f44336;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.btn-add {
-  background-color: #0077CC;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.form-group {
-  margin-bottom: 10px;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 5px;
-  color: #0b0b0b;
-}
-
-.form-group input[type="text"],
-.form-group input[type="number"] {
-  width: 100%;
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.form-group input:focus {
-  outline: none;
-  border-color: #0077CC;
-}
-</style>
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    font-family: sans-serif;
+  }
+  
+  .title {
+    margin-bottom: 20px;
+    text-align: center;
+    color: #0077CC;
+  }
+  
+  .card {
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+    padding: 20px;
+  }
+  
+  .list {
+    list-style: none;
+    padding: 0;
+  }
+  
+  .list-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+    padding: 10px 0;
+  }
+  
+  .list-item-content {
+    flex-grow: 1;
+  }
+  
+  .btn-delete {
+    background-color: #f44336;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  
+  .btn-add {
+    background-color: #0077CC;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  
+  .form-group {
+    margin-bottom: 10px;
+  }
+  
+  .form-group label {
+    display: block;
+    margin-bottom: 5px;
+    color: #0b0b0b;
+  }
+  
+  .form-group input[type="text"],
+  .form-group input[type="number"] {
+    width: 100%;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  .form-group input:focus {
+    outline: none;
+    border-color: #0077CC;
+  }</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// eslint-disable-next-line
 const app = createApp(App)
+
 
 app.mount('#app')
